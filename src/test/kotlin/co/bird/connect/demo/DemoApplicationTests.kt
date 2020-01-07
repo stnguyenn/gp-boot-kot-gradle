e5237d8d8e8e604a663d34e9ecf0c65ca0ee5ad7
Nguyen, Stephen <Stephen.Nguyen@mastercard.com>
2020-01-07 13:38:35 -0600

    spring init

---
+++ src/test/kotlin/co/bird/connect/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package co.bird.connect.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
